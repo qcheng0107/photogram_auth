2c63f074d9453b97bd557f03531db12ac4f218bf
Carey Cheng <qcheng@chicagobooth.edu>
2017-08-16 19:10:36 +0000

    if

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
   
   def index
     @photos = Photo.all
-
+    @comment = Comment.new
     render("photos/index.html.erb")
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Rails.application.routes.draw do
   devise_for :users
     # READ
   get "/users", :controller => "users", :action => "index"
-  get "/users/:id ", :controller => "users", :action => "show"
+  get "/users/:id", :controller => "users", :action => "show"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
